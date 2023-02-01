d25683d97c96824f32d79e701a1318da78a6d0dc
ElkaKR <ponomarenkoalena555@gmail.com>
2023-02-01 12:26:13 +0200

    Create Spring Boot Project. Closes #1

---
+++ src/main/kotlin/learn/spring5jokesapp/Spring5JokesAppApplication.kt
@@ -0,0 +1,11 @@
+package learn.spring5jokesapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring5JokesAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring5JokesAppApplication>(*args)
+}
